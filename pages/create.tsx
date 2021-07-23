e74bf544c74d94713f6113867a3ab2bf91bba99f
moisesescarcega <moi.escarcega@outlook.es>
2021-07-23 14:52:35 -0500

    Tutorial prisma nextjs primera revision

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,4 +1,4 @@
-import ReactMarkdown, { useState } from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
 import Router from "next/router";
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,18 +14,6 @@ export const getStaticProps: GetStaticProps = async () => {
       },
     },
   })
-  // const feed = [
-  //   {
-  //     id: 1,
-  //     title: "Prisma is the perfect ORM for Next.js",
-  //     content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
-  //     published: false,
-  //     author: {
-  //       name: "Nikolas Burk",
-  //       email: "burk@prisma.io",
-  //     },
-  //   },
-  // ]
   return { props: { feed } }
 }
 
